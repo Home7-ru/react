f4ae375f52c295b57563f4313d9f98b0083c4905
a.soluyanov <a--b--s@yandex.ru>
2019-10-31 18:00:38 +0400

    дерево элементов

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+//import React from 'react';
+//необходим для трансформации jsx кода
+//по типу
+//const el= React.createElement('h1', null, 'Hello World');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//мы создали элемент внутри которого есть другие элементы
+//таким образом мы создали дерево элементов
+const el = (
+    <div>
+        <h1>My Todo list</h1>
+        <input placeholder="search"/>
+        <ul>
+            <li> Первая строка</li>
+            <li> Вторая строка</li>
+        </ul>
+    </div>
+)
 
-const el = <h1>Hello World</h1>
 
-// el является обьетом можно посмотреть в консоли
-console.log(el);
 
 
 ReactDOM.render(el, document.getElementById('root'));
