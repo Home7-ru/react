89abc3c032147f3a19bfe59814b41d9ddea6ae67
a.soluyanov <a--b--s@yandex.ru>
2019-10-31 18:06:31 +0400

    создали первый компанент

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-//import React from 'react';
-//необходим для трансформации jsx кода
-//по типу
-//const el= React.createElement('h1', null, 'Hello World');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//мы создали элемент внутри которого есть другие элементы
-//таким образом мы создали дерево элементов
-const el = (
-    <div>
-        <h1>My Todo list</h1>
-        <input placeholder="search"/>
+//компанент это функция которая возвращает react элемент
+const TodoList=()=>{
+    return(
         <ul>
             <li> Первая строка</li>
             <li> Вторая строка</li>
         </ul>
+    );
+};
+
+const el = (
+    <div>
+        <h1>My Todo list</h1>
+        <input placeholder="search"/>
+        <TodoList/>
     </div>
 )
 
