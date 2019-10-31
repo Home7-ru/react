b37e4c208e8032dafe726967c5fc23b7f0b7e446
a.soluyanov <a--b--s@yandex.ru>
2019-10-31 17:40:46 +0400

    без jsx hello world

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>
+//синтаксис jsx
+//const el = <h1>Hello World</h1>
+
+//без jsx
+const el= React.createElement('h1', null, 'Hello World');
 
 ReactDOM.render(el, document.getElementById('root'));
