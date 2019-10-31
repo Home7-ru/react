6c7950851e33a7054e48e1f4e566a07efb32c0d5
a.soluyanov <a--b--s@yandex.ru>
2019-10-31 17:44:29 +0400

    разъяснение что jsx есть элемент

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//синтаксис jsx
-//const el = <h1>Hello World</h1>
 
-//без jsx
-const el= React.createElement('h1', null, 'Hello World');
+const el = <h1>Hello World</h1>
+
+// el является обьетом можно посмотреть в консоли
+console.log(el);
+
 
 ReactDOM.render(el, document.getElementById('root'));
