4b56157c1e7aacc87bc7208fc682500eab203ea7
a.soluyanov <a--b--s@yandex.ru>
2019-11-01 12:04:36 +0400

    разбили все компаненты  на файлы

---
+++ src/components/todo-list.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoList=()=>{
+
+    const items =['Первая строка1','Вторая строка'];
+
+    return(
+        <ul>
+            <li>cofe</li>
+            <li>cofe</li>
+        </ul>
+    );
+};
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-const TodoList=()=>{
-
-    const items =['Первая строка1','Вторая строка'];
-
-    return(
-        <ul>
-            <li> { items[0] }</li>
-            <li> { items[1] }</li>
-        </ul>
-    );
-};
-
-const AppHeader=()=>{
-    return(
-        <h1>My Todo list</h1>
-    );
-};
-
-const SearchPanel=()=>{
-
-    const searchText='Type here to search';
-    const searchStyle ={
-        fontStyle:'25px'
-    }
-    return(
-        <input
-            style={ searchStyle }
-            placeholder={searchText} />
-    );
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App=()=>{
-    //выдаст на экране текст тоесть передать команды так нельзя
-    const value ='<script>alert("")</script>';
+
+
     return(
         <div>
-            {value}
+
             <AppHeader/>
             <SearchPanel/>
             <TodoList/>
