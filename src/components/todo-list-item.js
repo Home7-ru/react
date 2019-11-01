81e974b1041ab77944bac4445cd013ca2ea3a956
a.soluyanov <a--b--s@yandex.ru>
2019-11-01 18:33:11 +0400

    изменяем свойства

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -5,11 +5,14 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component{
 
     state = {
-            done:false
+            done: false
         };
-    
+    //после инициализации state его можно лишь читать но не изменять на прямую
+    //для изменения используют setState
     onLabelClick = () => {
-        console.log(`элемент: ${this.props.label}`)
+        this.setState({
+            done:true
+        });
     };
 
 
@@ -17,8 +20,13 @@ export default class TodoListItem extends Component{
     render(){
 
         const { label, important = false } = this.props;
+        const { done } = this.state;
 
+        let classNames ='todo-list-item';
 
+        if(done){
+            classNames += ' done';
+        }
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -28,7 +36,7 @@ export default class TodoListItem extends Component{
 
 
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
       <span
           className="todo-list-item-label"
           style={style}
