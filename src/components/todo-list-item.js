32f12e7b2a46da9fe67eb38279594e90433a72a9
a.soluyanov <a--b--s@yandex.ru>
2019-11-01 13:06:28 +0400

    prosp продолжение

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 //передали масив, через диструктуризацию смотреть урок по диструктуризации
-const TodoListItem =({label})=>{
-    return <li>{label}</li>
+// в данном примере {label, important = false} по сути является props
+const TodoListItem =({label, important = false})=>{
+
+    const style ={
+        color: important ? 'tomato' : 'black'
+    };
+
+    return <span style={style}> {label}</span>;
 };
 
 export default TodoListItem;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,12 +2,14 @@ import React from "react";
 import TodoListItem from './todo-list-item';
 const TodoList=()=>{
 
-    const items =['Первая строка1','Вторая строка'];
+    //const items =['Первая строка1','Вторая строка'];
 
     return(
         <ul>
             <li><TodoListItem label="Drink Coffe"/></li>
-            <li><TodoListItem label="Build black"/></li>
+            <li><TodoListItem
+                label="Build black"
+                important /></li>
         </ul>
     );
 };
