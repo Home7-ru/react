d17f95f4f9596c612704ee44e8f23a91242e21b1
a.soluyanov <a--b--s@yandex.ru>
2019-11-02 11:27:19 +0400

    продолжаем добовлять события для del

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TodoListItem from './todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
 
   const elements = todos.map((item) => {
     const { id, ...itemProps } = item;
@@ -11,7 +11,7 @@ const TodoList = ({ todos }) => {
     return (
       <li key={id} className="list-group-item">
         <TodoListItem {...itemProps }
-        onDeleted={()=>console.log('deleted')}
+        onDeleted={()=>onDeleted(id)}
         />
       </li>
     );
--- src/index.js
+++ src/index.js
@@ -10,24 +10,26 @@ import './index.css';
 
 const App = () => {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Make Awesome App', important: true, id: 2},
+        {label: 'Have a lunch', important: false, id: 3}
+    ];
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
+    return (
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel/>
+                <ItemStatusFilter/>
+            </div>
 
-      <TodoList todos={todoData} />
-    </div>
-  );
+            <TodoList todos={todoData}
+                      onDeleted={ (id) => console.log('del',id)}
+            />
+        </div>
+    );
 };
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+ReactDOM.render(<App/>,
+    document.getElementById('root'));
