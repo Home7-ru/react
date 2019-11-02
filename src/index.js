709533195a9bb79818e36f6fdf0499e8781622ce
a.soluyanov <a--b--s@yandex.ru>
2019-11-02 16:49:13 +0400

    второй пример с массивом

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-// выберет 1 и 5
-const fib = [1, 1, 2, 3, 5, 8, 13];
-const [, a, , , b] = fib;
-console.log(a, b);
+// выберет 10 17 18 9
+const line = [[10, 17], [14, 7], [18, 9]];
+const [[p1x, p1y], ,[p2x, p2y]] = line;
+
+console.log(p1x, p1y, p2x, p2y);
+
+// выберет 10 17 14 7
+const line1 = [[10, 17], [14, 7], [18, 9]];
+const [[s1x, s1y], [s2x, s2y]] = line1;
+
+console.log(s1x, s1y, s2x, s2y);
