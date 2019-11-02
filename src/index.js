fa0b96a5cf47d504c35216dc1bf44a2e15419fcc
a.soluyanov <a--b--s@yandex.ru>
2019-11-02 16:49:45 +0400

    3 пример с массивом

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-// выберет 10 17 18 9
-const line = [[10, 17], [14, 7], [18, 9]];
-const [[p1x, p1y], ,[p2x, p2y]] = line;
-
-console.log(p1x, p1y, p2x, p2y);
-
-// выберет 10 17 14 7
-const line1 = [[10, 17], [14, 7], [18, 9]];
-const [[s1x, s1y], [s2x, s2y]] = line1;
-
-console.log(s1x, s1y, s2x, s2y);
+// выберет chris sandra guest
+const people = ['chris', 'sandra'];
+const [a1, b1, c1 = 'guest'] = people;
+console.log(a1, b1, c1);
