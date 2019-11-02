ae097c149ce4286aa14447ae72f1ed0cb8c721c6
a.soluyanov <a--b--s@yandex.ru>
2019-11-02 11:08:42 +0400

    убирем this.props и добавим через диструктуризацию

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -32,7 +32,7 @@ export default class TodoListItem extends Component{
 
     render(){
 
-        const { label } = this.props;
+        const { label, onDeleted } = this.props;
         const { done, important } = this.state;
 
         let classNames ='todo-list-item';
@@ -63,7 +63,7 @@ export default class TodoListItem extends Component{
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right"
-              onClick={this.props.onDeleted} >
+              onClick={onDeleted} >
         <i className="fa fa-trash-o" />
       </button>
     </span>
