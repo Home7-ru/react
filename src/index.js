b5f40882d253a22a44576ac2b2108060e6758fda
a.soluyanov <a--b--s@yandex.ru>
2019-11-03 14:46:34 +0400

    10-js объект из 2

--- src/index.js
+++ src/index.js
@@ -1,33 +1,3 @@
-const x=10;
-const y=30;
-//было раньше
-const point1={
-    x:x,
-    y:y
-
-};
-//стало сейчас
-const p1={x,y};
-//---------------------------
-//было раньше
-const point={
-    x:x,
-    y:y,
-    draw: function () {
-        //
-    }
-};
-//стало сейчас
-const p={x,y, draw(){}};
-
-//-------------------------------
-const prefix='_blah_';
-const data={
-    [prefix+'name']:'bob',
-    [prefix+'age']:23
-};
-//{ _blah_name: 'bob', _blah_age: 23 }
-
 const defaults={
     host:'localhost',
     dbName: 'blog',
@@ -37,36 +7,19 @@ const opts={
     user:'john',
     password: 'utopia'
 };
-//перезапишет defaults совместив с opts
-Object.assign(defaults,opts);
-
-
-
-/*{ host: 'localhost',
-  dbName: 'blog',
-  user: 'john',
-  password: 'utopia' }
-  */
-const defaults1={
-    host:'localhost',
-    dbName: 'blog',
-    user:'admin'
-};
-const opts1={
-    user:'john',
-    password: 'utopia'
-};
 //так создаем обьедененый обект из двух
 const res=Object.assign({},defaults,opts);
 
-//------------------------------
-const person={
-    name:'Bob',
-    friends:['Mark','Jacob']
-};
+const res1={...defaults,...opts};
+//
+const port=8080;
 
-//------Зделать копию обьекта обьект получается сылочный
-const shallowCopy=Object.assign({},person);
-// если в новый обьект добавть новое имя он появться и в дублированном обьекте
-person.friends.push('jane');
-console.log(shallowCopy);
+const res2={
+    ...defaults,
+    ...port,
+    port,
+    connect(){
+
+    }
+};
+console.log(res1);
