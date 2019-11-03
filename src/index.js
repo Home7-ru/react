6a18296e9b6952e59c09d11d9976a73957df6977
a.soluyanov <a--b--s@yandex.ru>
2019-11-03 14:38:46 +0400

    3-js Параметры по умолчанию

--- src/index.js
+++ src/index.js
@@ -1,76 +1,25 @@
 /**
  * Created by SadAdmin on 12.01.2019.
  */
-//функции по новому
-//Так Выглядела
-function sqr(){
-    return x*x;
-}
-//так можно записать
-const sqr =(x)=> x*x;
+//Параметры по умолчанию
+//было так
+function fetchOrders(count,start){
+    if(count===undefined){
+        count =10;
 
-//console.log(sq(2));
-//------------------------------------------------------
-const arr=['1','3','4','2'];
-
-//так было
-const res2= arr
-    .map(function (el) {
-        return parseInt(el);
-    })
-    .filter(function(num){
-        return num%2;
-    })
-    .reduce(function (max, value) {
-        return Math.max(max,value);
-    },0);
-//так стало
-const res=arr
-    .map((el)=>parseInt(el))
-    .filter((num)=>num%2)
-    .reduce((max,value)=>Math.max(max,value))
-
-//console.log(res);
-//console.log(res2);
-//-------------------------------------------------
-// данный код выдаст ошибку так как  this вызывается внутри функции
-/*
-const greeter ={
-    greet: function(name){
-        console.log('hello',name);
-    },
-    greetAll: function(names) {
-        names.forEach(function (name) {
-            this.greet(name);
-        });
     }
-    };
-greeter.greetAll(['bob','mark','pate']);
-    */
-
-const greeter ={
-    greet: function(name){
-        console.log('hello',name);
-    },
-    greetAll: function(names) {
-        names.forEach((name)=>{
-            this.greet(name);
-        });
+    if(start===undefined){
+        start=0;
     }
-};
-greeter.greetAll(['bob','mark','pate']);
-
-
-//--------------------------------------------------------
-//в простых функциях можно назначать прототип
-function Parson(){
-
 }
-parson.prototype.hi=function(){
-
+//стало так
+function fetchOrders(count=10,start=0){
+    console.log('count=',count,'start=',start);
 }
-// на функциях со => нельзя создать новый обьект
-const Person=()=>{
+//fetchOrders(15);
 
-};
-new Parson();
+// так можно установить обьект в пораметрах по умолчанию
+function findProducts(opts={ minPrice: 10, maxPrice: 20}){
+    console.log(opts);
+}
+findProducts();
