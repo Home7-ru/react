9ffe859f5e8fba9d4dfd061720d7a7ed2a9ac9ef
a.soluyanov <a--b--s@yandex.ru>
2019-11-03 14:35:48 +0400

    1-js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+//Ключевые слова let и const вместо var
+//let и const видны только в облости кода где они обьявлены
+//в отличии от var
+//пример let обьевляет переменную внутри снаружи не видна
+if(true){
+    let a=1;
+}
 
-console.log("пустой js");
+let name ='joe';
+name ='bob';
+const names=['peter','bob'];
+names.push('jen')
