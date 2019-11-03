5a46764bc4fc899f3902336e3722dcf7d06151ec
a.soluyanov <a--b--s@yandex.ru>
2019-11-03 14:36:34 +0400

    2-js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
-//Ключевые слова let и const вместо var
-//let и const видны только в облости кода где они обьявлены
-//в отличии от var
-//пример let обьевляет переменную внутри снаружи не видна
-if(true){
-    let a=1;
+/**
+ * Created by SadAdmin on 12.01.2019.
+ */
+//функции по новому
+//Так Выглядела
+function sqr(){
+    return x*x;
 }
+//так можно записать
+const sqr =(x)=> x*x;
 
-let name ='joe';
-name ='bob';
-const names=['peter','bob'];
-names.push('jen')
+//console.log(sq(2));
+//------------------------------------------------------
+const arr=['1','3','4','2'];
+
+//так было
+const res2= arr
+    .map(function (el) {
+        return parseInt(el);
+    })
+    .filter(function(num){
+        return num%2;
+    })
+    .reduce(function (max, value) {
+        return Math.max(max,value);
+    },0);
+//так стало
+const res=arr
+    .map((el)=>parseInt(el))
+    .filter((num)=>num%2)
+    .reduce((max,value)=>Math.max(max,value))
+
+//console.log(res);
+//console.log(res2);
+//-------------------------------------------------
+// данный код выдаст ошибку так как  this вызывается внутри функции
+/*
+const greeter ={
+    greet: function(name){
+        console.log('hello',name);
+    },
+    greetAll: function(names) {
+        names.forEach(function (name) {
+            this.greet(name);
+        });
+    }
+    };
+greeter.greetAll(['bob','mark','pate']);
+    */
+
+const greeter ={
+    greet: function(name){
+        console.log('hello',name);
+    },
+    greetAll: function(names) {
+        names.forEach((name)=>{
+            this.greet(name);
+        });
+    }
+};
+greeter.greetAll(['bob','mark','pate']);
+
+
+//--------------------------------------------------------
+//в простых функциях можно назначать прототип
+function Parson(){
+
+}
+parson.prototype.hi=function(){
+
+}
+// на функциях со => нельзя создать новый обьект
+const Person=()=>{
+
+};
+new Parson();
