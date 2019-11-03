a71ac3024b4635248f46b2c94809402059945f15
a.soluyanov <a--b--s@yandex.ru>
2019-11-03 14:45:00 +0400

    8-js строковая переменная по новому

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-//Array Destructuring
-const fib = [1, 1, 2, 3, 5, 8, 13];
-const [, a, , b] = fib;
-console.log(a, b);
+const user ='bob';
+const num=17;
+//было
+const txt='hello'+user+'you have'+num+'letters';
 
-const line = [[10, 17], [14, 7]];
-const [[p1x, p1y], [p2x, p2y]] = line;
+//стало
+const tst2=`heloo ${user} you ${num} letters`;
 
-console.log(p1x, p1y, p2x, p2y);
+// можно выводить и значения с функциями
 
-const people = ['chris', 'sandra'];
-const [a1, b1, c1 = 'guest'] = people;
-console.log(a1, b1, c1);
+const tst3=`now is ${Date.now()}`;
 
-const people1 = ['chris', 'sandra', 'bob'];
-const [a2, ...others] = people1;
-console.log(others);
+console.log(tst3);
 
-const dict = {
-    duck: 'quack',
-    dog: 'wuff',
-    mouse: 'squeak',
-    hamster: "squeak"
-}
-// выберет все элементы с 'squeak'
-const res = Object.entries(dict)
-    .filter((arr) => arr[1] === 'squeak');
-console.log(res)
+const html=
+    '<ul>'+
+    '<li>Item One</li>'+
+    '<li>Item Two</li>'+
+    '</ul>';
+const items=['tea','coffee'];
+const tamplateHtml=`'<ul>
+    <li>${items[0]}</li>
+    <li>Item Two</li>
+    </ul>`;
+console.log(tamplateHtml);
