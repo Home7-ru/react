b799d813dcea40691497bfbab87804cfd0939432
a.soluyanov <a--b--s@yandex.ru>
2019-11-04 15:27:57 +0400

    in-line стили jsx предподчтительный вариант

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-//так как это jsx то мы передаем объект
+
     const divStyle={
-      'text-align':'center'
+      textAlign:'center'
     }
-// внутри jsx можно писать так
+
     return (
 
          <div style={divStyle}>
-            <h1 style={{color:'blue','font-size': '20px'}}>Hello world!</h1>
+            <h1 style={{color:'blue',fontSize: '20px'}}>Hello world!</h1>
          </div>
 
 
