926f30e611289147fba8da14e29ef77ff8045acd
a.soluyanov <a--b--s@yandex.ru>
2019-11-04 16:52:54 +0400

    различные варианты создания компанента

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-function Car(){
-    return(
-        <h2>This is car component</h2>
-    )
-}
+// function Car(){
+//     return(
+//         <div>This is car component</div>
+//     )
+// }
+
+
+//можно так
+// const Car =()=>{
+//     return(
+//         <div>This is car component</div>
+//     )
+// }
+
+//можно так
+
+//const Car =()=>(<div>This is car component</div>)
+
+//a можно так
+const Car =()=>(
+    <div>This is car component
+    <strong>test</strong>
+    </div>
+
+)
 
 export default Car
