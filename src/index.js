94b432943ec52777a73cfeea3045215a157cf161
a.soluyanov <a--b--s@yandex.ru>
2019-11-04 12:28:26 +0400

    начальный шаблон

--- src/index.js
+++ src/index.js
@@ -1,76 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemAddForm from './components/item-add-form';
-import ItemStatusFilter from './components/item-status-filter';
-
 import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-export default class App extends Component {
-
-    maxId=100;
-
-    state= {
-        todoData : [
-            {label: 'Drink Coffee', important: false, id: 1},
-            {label: 'Make Awesome App', important: true, id: 2},
-            {label: 'Have a lunch', important: false, id: 3}
-        ]
-    };
-
-    deleteItem =(id)=>{
-        this.setState(({todoData})=>{
-
-            const idx = todoData.findIndex((el) => el.id === id);
-
-            //конструируем новый масив из всех элементов до нужного и после нужного
-            const newArray=[...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
-
-            //возврашаем новое состояние
-
-            return{
-                todoData: newArray
-            }
-        });
-    };
-
-    addItem =(text)=>{
-         const newItem={
-             label:text,
-             important:false,
-             id:this.maxId++
-         };
-         this.setState(({todoData}) => {
-             const newArr=[
-                 ...todoData,
-                 newItem
-             ];
-             return{
-                 todoData: newArr
-             }
-        });
-    };
-
-    render() {
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel/>
-                <ItemStatusFilter/>
-            </div>
-
-            <TodoList todos={this.state.todoData}
-                      onDeleted={this.deleteItem}
-            />
-            <ItemAddForm onItemAdded={this.addItem}/>
-        </div>
-        );
-    }
-};
-
-ReactDOM.render(<App/>,
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
