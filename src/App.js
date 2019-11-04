dc271306cd9fc6bcce841165b660f3398fc26996
a.soluyanov <a--b--s@yandex.ru>
2019-11-04 15:10:53 +0400

    1 ограничение jsx

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+
+// нужно использовать className вместо class в css
+
       <div className="App">
         <h1>Hello world!</h1>
       </div>
     );
-//на самом деле вызывается вот такой код
-    // return React.createElement(
-    //   'div',
-    //   {
-    //     className: 'App'
-    //   },
-    //   React.createElement(
-    //     'h1',
-    //     null,
-    //     'Hello world!'
-    //   )
-    // )
+
   }
 }
 
