c9c1645f065814a646131bf930528fd334aa9350
a.soluyanov <a--b--s@yandex.ru>
2019-11-06 19:17:20 +0400

    добавили обработчик только для одного компанента

--- src/App.js
+++ src/App.js
@@ -39,9 +39,17 @@ class App extends Component {
             <h1>{this.state.pageTitle}</h1>
 
            <button onClick={this.changeTitleHandker}>Change title</button>
-           <Car name={cars[0].name} year={cars[0].year}/>
-           <Car name={cars[1].name} year={cars[1].year}/>
-           <Car name={cars[2].name} year={cars[2].year}/>
+           <Car
+               name={cars[0].name}
+               year={cars[0].year}
+               onChangeTitle={this.changeTitleHandker}
+           />
+           <Car
+               name={cars[1].name}
+               year={cars[1].year}/>
+           <Car
+               name={cars[2].name}
+               year={cars[2].year}/>
          </div>
 
 
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 // может быть так а может быть и без скобочек
 // const Car =(props)=>(
-const Car =props=>(
+const Car = props =>(
     <div>
         <h3>Car name:{props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
+        <button onClick={props.onChangeTitle}>Click</button>
 
     </div>
 
