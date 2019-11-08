1a060a6f5f2538ff625261111945be78aa970fdd
a.soluyanov <a--b--s@yandex.ru>
2019-11-08 15:44:32 +0400

    выносим во вне css

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,16 +1,9 @@
 import React from 'react';
-
+import './Car.css'
 // может быть так а может быть и без скобочек
 // const Car =(props)=>(
 const Car = props =>(
-    <div style={{
-        border:'1px solid #ccc',
-        marginBottom: '10px',
-        display: 'block',
-        padding: '10px',
-        boxShadow: '10px 1px 1px red',
-        borderRadius: '5px'
-    }}>
+    <div className="Car">
         <h3>Car name:{props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
         <input type="text" onChange={props.onChangeName} value={props.name}/>
