231c5d3c15d888012c2f6a3f522aaf0b4b15a735
a.soluyanov <a--b--s@yandex.ru>
2019-11-08 16:02:49 +0400

    добавили реакцию на input

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './Car.css'
 // может быть так а может быть и без скобочек
 // const Car =(props)=>(
-const Car = props =>(
+const Car = props =>{
+
+    const inputClasses=['input']
+
+    if (props.name !== ''){
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    return (
     <div className="Car">
         <h3>Car name:{props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <input type="text" onChange={props.onChangeName} value={props.name} className={inputClasses.join(' ')}/>
         <button onClick={props.onDelete}>Delete</button>
 
     </div>
-
-
 )
+
+}
 export default Car
