66c08ae26a4c4e55b7758bc7e61a6eae5a3f3667
a.soluyanov <a--b--s@yandex.ru>
2019-11-12 12:21:21 +0400

    добавили radium компанент который добавляет новые фичи css в js

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Radium from 'radium'
 import './Car.css'
 // может быть так а может быть и без скобочек
 // const Car =(props)=>(
+
+
 const Car = props =>{
 
     const inputClasses=['input']
@@ -16,8 +19,17 @@ const Car = props =>{
         inputClasses.push('bold')
     }
 
+    const style={
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+        ':hover':{
+            border:'1px solid #aaa',
+            boxShadow:' 0 4px 15px 0 rgba(0,0,0, .25)'
+        }
+    }
+
     return (
-    <div className="Car">
+    <div className="Car" style={style}>
         <h3>Car name:{props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
         <input type="text" onChange={props.onChangeName} value={props.name} className={inputClasses.join(' ')}/>
@@ -27,4 +39,4 @@ const Car = props =>{
 )
 
 }
-export default Car
+export default Radium(Car)
